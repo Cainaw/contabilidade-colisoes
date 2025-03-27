fbc22ba0092e48ec88716fa37ad3f8676a786aa6
Cainã <cainamwc@gmail.com>
2025-03-27 08:45:34 -0300

    alteração na geração de lista

--- src/analise/Resultado.java
+++ src/analise/Resultado.java
@@ -8,17 +8,22 @@ import util.VetorRandomico;
 public class Resultado {
 
 	public static void main(String[] args) {
-		VetorRandomico vr = new VetorRandomico(1000);
+		VetorRandomico vr = new VetorRandomico();
+		vr.setLista(1000);
 		List<Integer> listaGerada = vr.getLista();
 		
 		Dobra dobra = new Dobra();
 		Murmur murmur = new Murmur();
 		
-		int colisoes = murmur.calculoColisoes(listaGerada);
+		int colisoesDobra = dobra.calculoColisoes(listaGerada);
+		int colisoesMurmur = murmur.calculoColisoes(listaGerada);
 		
 		//int colisoes = dobra.calculoColisoes(listaGerada);
 		
-		System.out.println("Total: " + colisoes);
+		System.out.println("Total: ");
+		System.out.println("Dobra: " + colisoesDobra);
+		System.out.println("Murmur: " + colisoesMurmur);
+		
 	}
 
 }
--- src/util/VetorRandomico.java
+++ src/util/VetorRandomico.java
@@ -8,16 +8,22 @@ import java.util.Set;
 public class VetorRandomico {
 	private List<Integer> lista;
 	
-	public VetorRandomico(Integer tamanho) {
+	public Boolean setLista(int tamanho) {
+		if (tamanho <= 0)
+			return false;
+		if (tamanho >= Integer.MAX_VALUE)
+			return null;
+		
 		Random aleatorio = new Random();
 		Set<Integer> listaTemporaria = new HashSet<>();
 		
         while (listaTemporaria.size() < tamanho) {
-            int valor = aleatorio.nextInt(tamanho * 1000);
+            int valor = aleatorio.nextInt(Integer.MAX_VALUE);
             listaTemporaria.add(valor);
         }
         
         lista = List.copyOf(listaTemporaria);
+        return true;
 	}
 	
 	public List<Integer> getLista() {
