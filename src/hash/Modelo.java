8061e258a2b5d92938b0bbcd274913e5c671d677
Cainã <cainamwc@gmail.com>
2025-03-27 07:50:34 -0300

    interface configurada

--- src/hash/Modelo.java
+++ src/hash/Modelo.java
@@ -1,5 +1,8 @@
 package hash;
 
-public interface Modelo {
+import java.util.List;
 
+public interface Modelo {
+	public int calculoColisoes(List<Integer> lista);
+	public int calculoIndice(Integer valor, int tamanho);
 }
